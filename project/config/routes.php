e87932e70fd076d02e6f451da6488db5e5921196
AndreyKlym <d3116280@gmail.com>
2023-01-11 21:52:24 +0100

    Using the MVC Framework

---
+++ project/config/routes.php
@@ -0,0 +1,19 @@
+<?php
+//⊗ppOpUFmCAR
+
+	use \Core\Route;
+	
+	return [
+		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
+		new Route('/my-page1/', 'page', 'show1'),
+		new Route('/my-page2/', 'page', 'show2'),
+
+//        task #1
+		new Route('/my-act1/', 'test', 'act1'),
+		new Route('/my-act2/', 'test', 'act2'),
+		new Route('/my-act3/', 'test', 'act3'),
+
+        new Route('/test/:var1/:var2/', 'page', 'act'),
+
+	];
+	
---
+++ project/controllers/TestController.php
@@ -0,0 +1,23 @@
+<?php
+//⊗ppOpUFmCAR
+//task #1
+	namespace Project\Controllers;
+	use \Core\Controller;
+
+    class TestController extends Controller
+    {
+        public function act1()
+        {
+            echo '111';
+        }
+
+        public function act2()
+        {
+            echo '222';
+        }
+
+        public function act3()
+        {
+            echo '333';
+        }
+	}
